55a4a532038e81ea70c54cb9b392b593cd34822b
jacko2611 <jacksonkent70@gmail.com>
2023-04-14 22:15:37 +1000

    changed directory structure and have written portfolio.js

---
+++ portfolio/src/components/Portfolio.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Portfolio() {
+  return (
+    <div>
+      <h1>Portfolio</h1>
+      <ul>
+        <li>
+          <a href="https://github.com/jacko2611/chat-support">Chat Support - A ticket based support application using Socket IO for communication</a>
+        </li>
+        <li>
+          <a href="https://github.com/jacko2611/just-another-editor">JATE - A text editor PWA</a>
+        </li>
+        <li>
+          <a href="https://github.com/jacko2611/staff-tracker">Staff Tracker - A CLI application to keep track of staff using Sequelize</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Portfolio;
