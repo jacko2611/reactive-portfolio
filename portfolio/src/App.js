448647901355041bc9e4950d87b3a4077f833d03
jacko2611 <jacksonkent70@gmail.com>
2023-04-14 21:53:02 +1000

    have written basics in app.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">About Me</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+            <li>
+              <Link to="/portfolio">Portfolio</Link>
+            </li>
+            <li>
+              <Link to="/resume">Resume</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route exact path="/" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/portfolio" component={Portfolio} />
+          <Route path="/resume" component={Resume} />
+        </Switch>
+      </div>
+    </Router>
   );
+}
+
+function About() {
+  return <h1>About Me</h1>;
+}
+
+function Contact() {
+  return <h1>Contact</h1>;
+}
+
+function Portfolio() {
+  return <h1>Portfolio</h1>;
+}
+
+function Resume() {
+  return <h1>Resume</h1>;
 }
 
 export default App;
