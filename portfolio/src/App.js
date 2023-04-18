d4cdf0821723b8aa34dfee027e2173fc149a40b5
jacko2611 <jacksonkent70@gmail.com>
2023-04-18 21:30:26 +1000

    added more detail and a photo to my about me

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -32,6 +32,20 @@ function App() {
           <Route path="/portfolio" element={<Portfolio />} />
           <Route path="/resume" element={<Resume />} />
         </Routes>
+
+        <footer>
+          <div className="social-links">
+            <a href="https://github.com/your-username" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-github" />
+            </a>
+            <a href="https://www.linkedin.com/in/your-username/" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-linkedin" />
+            </a>
+            <a href="https://stackoverflow.com/users/your-user-id/your-username" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-stack-overflow" />
+            </a>
+          </div>
+        </footer>
       </div>
     </Router>
   );
