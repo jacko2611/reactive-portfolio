d13d3dbb349e88176db3eef8e56c296bb9164824
jacko2611 <jacksonkent70@gmail.com>
2023-04-18 21:42:14 +1000

    have added to resume.js to include a downloadable pdf link

--- portfolio/src/components/Resume.js
+++ portfolio/src/components/Resume.js
@@ -4,8 +4,8 @@ function Resume() {
   return (
     <div>
       <h1>Resume</h1>
-      <p>Here's a link to my resume:</p>
-      <a href="https://example.com/resume.pdf">Resume</a>
+      <p>Here's a link to download my resume:</p>
+      <a href="/Users/jacksonkent/Desktop/Bootcamp/reactive-portfolio/portfolio/src/JacksonKResume.pdf" download="JacksonKResume.pdf">Resume</a>
     </div>
   );
 }
