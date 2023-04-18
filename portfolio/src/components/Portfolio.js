f823ef91898461cc31682ea4bc7da05f7ce6cf46
jacko2611 <jacksonkent70@gmail.com>
2023-04-18 11:30:46 +1000

    have added portfolio.css to create cards for each project

--- portfolio/src/components/Portfolio.js
+++ portfolio/src/components/Portfolio.js
@@ -1,19 +1,44 @@
 import React from 'react';
+import './Portfolio.css'; // import external CSS file for styling
 
 function Portfolio() {
   return (
-    <div>
-      <div style={{ display: 'inline-block', textAlign: 'center', margin: '0 20px' }}>
-        <img src={require("/Users/jacksonkent/Desktop/Bootcamp/reactive-portfolio/portfolio/src/image1.jpg")} style={{ display: 'block', margin: '0 auto', width: '50%' }} />
-        <a href="https://github.com/jacko2611/chat-support">Chat Support - A ticket based support application using Socket IO for communication</a>
+    <div className="portfolio-container">
+      <div className="portfolio-card">
+        <img src={require("/Users/jacksonkent/Desktop/Bootcamp/reactive-portfolio/portfolio/src/image1.jpg")} alt="Project 1" />
+        <div className="portfolio-description">
+          <a href="https://github.com/jacko2611/chat-support">Chat Support</a>
+        </div>
       </div>
-      <div style={{ display: 'inline-block', textAlign: 'center', margin: '0 20px' }}>
-        <img src={require("/Users/jacksonkent/Desktop/Bootcamp/reactive-portfolio/portfolio/src/image2.jpg")} style={{ display: 'block', margin: '0 auto', width: '50%' }} />
-        <a href="https://github.com/jacko2611/just-another-editor">JATE - A text editor PWA</a>
+      <div className="portfolio-card">
+        <img src={require("/Users/jacksonkent/Desktop/Bootcamp/reactive-portfolio/portfolio/src/image2.jpg")} alt="Project 2" />
+        <div className="portfolio-description">
+          <a href="https://github.com/jacko2611/just-another-editor">JATE</a>
+        </div>
       </div>
-      <div style={{ display: 'inline-block', textAlign: 'center', margin: '0 20px' }}>
-        <img src={require("/Users/jacksonkent/Desktop/Bootcamp/reactive-portfolio/portfolio/src/image3.jpg")} style={{ display: 'block', margin: '0 auto', width: '50%' }} />
-        <a href="https://github.com/jacko2611/staff-tracker">Staff Tracker - A CLI application to keep track of staff using Sequelize</a>
+      <div className="portfolio-card">
+        <img src={require("/Users/jacksonkent/Desktop/Bootcamp/reactive-portfolio/portfolio/src/image3.jpg")} alt="Project 3" />
+        <div className="portfolio-description">
+          <a href="https://github.com/jacko2611/staff-tracker">Staff Tracker</a>
+        </div>
+      </div>
+      <div className="portfolio-card">
+        <img src={require("/Users/jacksonkent/Desktop/Bootcamp/reactive-portfolio/portfolio/src/image4.jpg")} alt="Project 4" />
+        <div className="portfolio-description">
+          <a href="https://github.com/jacko2611/social-backend">Social Backend</a>
+        </div>
+      </div>
+      <div className="portfolio-card">
+        <img src={require("/Users/jacksonkent/Desktop/Bootcamp/reactive-portfolio/portfolio/src/image5.jpg")} alt="Project 5" />
+        <div className="portfolio-description">
+          <a href="https://github.com/jacko2611/shopfront-to-backend">Shopfront - Backend</a>
+        </div>
+      </div>
+      <div className="portfolio-card">
+        <img src={require("/Users/jacksonkent/Desktop/Bootcamp/reactive-portfolio/portfolio/src/image6.jpg")} alt="Project 6" />
+        <div className="portfolio-description">
+          <a href="https://github.com/jacko2611/are-you-reading.me">Are You Reading.me</a>
+        </div>
       </div>
     </div>
   );
